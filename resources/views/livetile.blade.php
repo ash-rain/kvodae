fbc2082a619cb8d3c32cf70d7fef251016fcb928
ashrain <balkanski@outlook.com>
2015-04-07 03:23:10 +0300

    Windows live tile fix

--- resources/views/livetile.blade.php
+++ resources/views/livetile.blade.php
@@ -1,16 +1,16 @@
 <tile>
    <visual lang="en-US" version="2">
       <binding template="TileSquare310x310ImageAndTextOverlay02" branding="name">
-         <image id="1" src="/images/{{ $product->images[0]->id }}" />
+         <image id="1" src="{{ url('images/' . $product->images[0]->id) }}" />
          <text id="1">{{ $product->name }} </text>
          <text id="2">{{ $product->finalPrice .' '. config('app.checkout_currency') }}</text>
       </binding>
       <binding template="TileWide310x150ImageAndText01" branding="logo">
-         <image id="1" src="/images/{{ $product->images[0]->id }}" />
+         <image id="1" src="{{ url('images/' . $product->images[0]->id) }}" />
          <text id="1">{{ $product->name }} </text>
       </binding>
       <binding template="TileSquare150x150Image" branding="name">
-         <image id="1" src="/images/{{ $product->images[0]->id }}" />
+         <image id="1" src="{{ url('images/' . $product->images[0]->id) }}" />
       </binding>
    </visual>
 </tile>
